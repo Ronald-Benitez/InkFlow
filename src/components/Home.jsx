51d9305288bc0e48f4bd4dfbf3875d566a8cb4a3
Empanada <ronald362001@gmail.com>
2023-08-20 23:38:10 -0600

    complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,24 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <Container fluid className="home-page p-5" style={{
+    <Container
+      fluid
+      className="home-page p-5"
+      style={{
         fontFamily: "Raleway",
         letterSpacing: 1,
         fontSize: 16,
         background: "#F4E7D5",
         height: "100vh",
-    }}>
+      }}
+    >
       <Row className="justify-content-center align-items-center">
-        <Col md={6} className="text-center">
+        <Col md={6} className="text-center pt-5">
           <img
-            src="logo-image.jpg" // Replace with the actual image URL
+            src="/logo2.svg"
             alt="InkFlow Services Logo"
             className="logo-image"
           />
-          <h1 className="company-name">InkFlow Services</h1>
           <p className="company-description">
             Tu socio confiable en mantenimiento y preparación de equipos de
             impresión. Con sede en San Miguel y alcance nacional.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,78 @@
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavBar = () => {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
 
+  const handleNavLinkClick = (sectionId) => {
+    // Cambiar el color de todos los enlaces a #E3B155
+    const navLinks = document.querySelectorAll(".nav-link");
+    navLinks.forEach((link) => {
+      link.style.color = "#E3B155";
+    });
+
+    // Cambiar el color del enlace seleccionado a #84B09B
+    const selectedLink = document.querySelector(`#${sectionId}-link`);
+    if (selectedLink) {
+      selectedLink.style.color = "#84B09B";
+    }
+
+    // Desplazar a la sección correspondiente
+    scrollToSection(sectionId);
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" sticky="top" style={{ backgroundColor: '#27242A' }}>
-      <Navbar.Brand href="#">Logo</Navbar.Brand>
-      <Nav className="ml-auto" style={{ justifyContent: 'flex-start' }}>
-        <Nav.Link onClick={() => scrollToSection('inicio')}>Inicio</Nav.Link>
-        <Nav.Link onClick={() => scrollToSection('servicio')}>Servicio</Nav.Link>
-        <Nav.Link onClick={() => scrollToSection('sobre-nosotros')}>Sobre nosotros</Nav.Link>
-        <Nav.Link onClick={() => scrollToSection('contacto')}>Contacto</Nav.Link>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      sticky="top"
+      style={{
+        backgroundColor: "#27242A",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <Navbar.Brand href="#" className="p-2">
+        Logo
+      </Navbar.Brand>
+      <Nav className="">
+        <Nav.Link
+          id="inicio-link"
+          onClick={() => handleNavLinkClick("inicio")}
+          className="nav-link"
+          style={{ color: "#E3B155" }}
+        >
+          Inicio
+        </Nav.Link>
+        <Nav.Link
+          id="servicio-link"
+          onClick={() => handleNavLinkClick("servicio")}
+          className="nav-link"
+          style={{ color: "#E3B155" }}
+        >
+          Servicio
+        </Nav.Link>
+        <Nav.Link
+          id="sobre-nosotros-link"
+          onClick={() => handleNavLinkClick("sobre-nosotros")}
+          className="nav-link"
+          style={{ color: "#E3B155" }}
+        >
+          Sobre nosotros
+        </Nav.Link>
+        <Nav.Link
+          id="contacto-link"
+          onClick={() => handleNavLinkClick("contacto")}
+          className="nav-link"
+          style={{ color: "#E3B155" }}
+        >
+          Contacto
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
